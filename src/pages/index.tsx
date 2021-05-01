34b3b58ec5a0143d3810f2c133f6509f9f693166
alfredots <tito.alfred@hotmail.com>
2021-05-01 10:14:47 -0300

    fix - verificando erro do google button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,6 +24,7 @@ export default function Home() {
   }
 
   function responseGoogle(response) {
+    console.log(response)
     saveUser({
       name: response.ft.Te,
       username: response.ft.Qt,
